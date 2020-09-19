793148510bfff887b7da905fcb7b96b5c7693e68
Jackie <taekwondo.chan@gmail.com>
2020-09-19 16:58:50 -0700

    cart functional

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -54,6 +54,29 @@ all_items_button.forEach(elt => elt.addEventListener('click', () => {
 
 const cart = []
 
+itemList.onchange = function(e) {
+  if (e.target && e.target.classList.contains("update")) {
+    const name = e.target.dataset.name
+    const qty = parseInt (e.target.value)
+    updateCart(name,qty)
+  }
+}
+
+itemList.onclick = function(e) {
+  console.log("Clicked List!")
+  if (e.target && e.target.classList.contains("remove")) {
+    const name = e.target.dataset.name
+    removeItem(name)
+  } else if (e.target && e.target.classList.contains("add-1")) {
+      const name = e.target.dataset.name
+      addItem(name)
+  } else if (e.target && e.target.classList.contains("remove-1")) {
+      const name = e.target.dataset.name
+      removeItem(name, 1)
+   }
+  }
+}
+
 //Add Item
 function addItem(name, price) {
   for (let i=0; i<cart.length; i+=1) {
@@ -79,6 +102,10 @@ function showItems() {
     itemStr += `<li>${name } 
     $${price} x ${qty} = 
     ${qty * price}
+    <button class="remove" data-name="${name}">Remove</button>
+    <button class="add-1" data-name="${name}"> + </button>
+    <button class="remove-1" data-name="${name}"> - </button>
+    <input class ="update" type="number" data-name="${name}">
     </li>`
   }
 
@@ -115,9 +142,25 @@ function removeItem(name, qty = 0) {
             if (cart[i].qty < 1 || qty === 0){
                 cart.splice(i,1)
             }
+            showItems()
             return 
         }
     }
+}
+
+function updateCart(name,qty) {
+  for (let i=0; i< cart.length; i+=1) {
+    if (cart[i].name === name) {
+      if (qty < 1) {
+        removeItem(name)
+        return
+      }
+      cart[i].qty = qty
+      showItems()
+        return
+      )
+    }
+  }
 }
 //-------------------------------------
 addItem("Apple", 0.99)
